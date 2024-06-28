591fa89a26bf26e80622aa0e9170fd05520ada22
Samu <samu@hotmail.com>
2024-06-28 18:02:17 +0100

    Our second commit

---
+++ sam.java
@@ -0,0 +1,8 @@
+public class Sam{
+	public static void main(String[] args){
+
+System.out.println("Hello Sam, How are you today?");
+
+System.out.println("Hello, I am very well thank, and you?");
+	}
+}
--- sam.js
+++ sam.js
@@ -4,3 +4,4 @@ window.alert("You are about to see the text about my website");
 document.getElementById('samTxt1').textContent="I dedicate this work to my family, especially my Sweetie J."
 document.getElementById("samTxt2").textContent="Because she means a lot to me Because she means a lot to me Because she means a lot to meBecause she means a lot to me"
 document.getElementById('samTxt3').textContent="I am just testing."
+document.getElementById('samTxt4').textContent="Things are getting better now."
